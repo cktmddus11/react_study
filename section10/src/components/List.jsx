41fcc1ba707e188fa653e8108ee73e2fc6d485e7
cktmddus1111 <cktmddus1111@naver.com>
2024-08-08 23:14:45 +0900

    10 최적화

---
+++ section10/src/components/List.jsx
@@ -0,0 +1,59 @@
+import TodoItem from './TodoItem';
+import { useState, useMemo } from 'react';
+import './css/List.css';
+
+
+const List = ({ todos, onUpdate, onDelete }) => {
+    const [search, setSearch] = useState("");
+
+    const getAnalyzedData = () => { // 검색할 때는 렌더링 되며 재호출될필요없음. 이런경우 useMemo
+        console.log("getAnalyzedData() call");
+        const totalCount = todos.length;
+        const doneCount = todos.filter((todo) => todo.isDone).length;
+        const notDoneCount = todos.filter((todo) => !todo.isDone).length;
+
+        return { totalCount, doneCount, notDoneCount };
+    }
+
+    const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+        return getAnalyzedData();
+    }, [todos]);
+    // 의존성 배열 : deps
+    // useEffect 와 같이 deps값이 바뀌면 callback를 다시 실행하는 훅.
+    // 1. callback 함수결과 그대로 반환
+    // 2. 첫번째 전달한 callback함수를 deps를 기준으로 메모리제이션함.
+
+    // const {totalCount, doneCount, notDoneCount } = getAnalyzedData()
+
+    const onSearchChange = (e) => {
+        const searchValue = e.target.value;
+        setSearch(searchValue);
+    }
+
+    const filteredTodos = () => {
+        if (search == "") {
+            return todos;
+        }
+        return todos.filter((todo) =>
+            todo.content.toLowerCase().includes(search.toLowerCase()))
+    }
+    const filterTodos = filteredTodos();
+
+    return <div className='List'>
+        <h4>Todo List 🌱</h4>
+        <div>totalCount : {totalCount}</div>
+        <div>done : {doneCount}</div>
+        <div>not done : {notDoneCount}</div>
+
+
+        <input placeholder="검색어를 입력하세요" onChange={onSearchChange} />
+        <div className="todo_wrapper">
+            {filterTodos.map((todo, index) => (
+                <TodoItem {...todo} onUpdate={onUpdate} onDelete={onDelete} />
+            ))}
+
+        </div>
+
+    </div>
+}
+export default List;
---
+++ section10/src/data/mockData.js
@@ -0,0 +1,21 @@
+const mockData = [
+    {
+      id: 0,
+      isDone: false,
+      content: "React 공부하기",
+      date: new Date().getTime(),
+    },
+    {
+      id: 1,
+      isDone: true,
+      content: "하남가기",
+      date: new Date().getTime(),
+    },
+    {
+      id: 2,
+      isDone: false,
+      content: "노트북 세팅하기",
+      date: new Date().getTime(),
+    },
+  ];
+  export default mockData;
