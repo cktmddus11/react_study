3be93ef504da7d6cceb92086bb4ded8b7b13bd5a
cktmddus11 <cktmddus11@naver.com>
2024-08-20 22:30:07 +0900

    12.8) 일기 관리 기능 구현하기 1 초기 세팅

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -2,42 +2,22 @@ import './App.css';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
+import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
-
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
-
-import { getEmotionImage } from './util/get-emotion-image.js';
-
-import Button from './components/Button';
+import { Routes, Route } from 'react-router-dom';
 
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  {/* 주의 
-     - Routes 컴포넌트 안에는 Route 컴포넌트만 호출할 수 있다. 
-     - Routes 컴포넌트 밖에 추가한 것들은 모든 페이지에 렌더링된다. 
-    */}
-  const nav = useNavigate();
-  const onClickButton = () => {
-    nav("/new");
-  }
-
   return (
     <>
-      <Button text="완전 좋음" type={'DEFAULT'} />
-      <Button text="좋음" type={'POSITIVE'} />
-      <Button text="그저그럼" type={'NORMAL'} />
-      <Button text="나쁨" type={'BAD'} />
-      <Button text="나쁨" type={'NAGATIVE'} />
-
-
-
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/new" element={<New />}></Route>
         <Route path="/diary/:id" element={<Diary />}></Route>
+        <Route path="/edit/:id" element={<Edit />}></Route>
         <Route path="*" element={<Notfound />}></Route>
       </Routes>
     </>
@@ -46,8 +26,29 @@ function App() {
 
 export default App
 
+// import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+
+// import { getEmotionImage } from './util/get-emotion-image.js';
+
+// import Button from './components/Button';
+  {/* 주의 
+     - Routes 컴포넌트 안에는 Route 컴포넌트만 호출할 수 있다. 
+     - Routes 컴포넌트 밖에 추가한 것들은 모든 페이지에 렌더링된다. 
+    */}
+    // const nav = useNavigate();
+    // const onClickButton = () => {
+    //   nav("/new");
+    // }
 // return (
 //   <>
+      {/* <Button text="완전 좋음" type={'DEFAULT'} />
+      <Button text="좋음" type={'POSITIVE'} />
+      <Button text="그저그럼" type={'NORMAL'} />
+      <Button text="나쁨" type={'BAD'} />
+      <Button text="나쁨" type={'NAGATIVE'} /> */}
+
+
+
 //     <div>
 //       <img src={getEmotionImage(1)} />
 //       <img src={getEmotionImage(2)} />
